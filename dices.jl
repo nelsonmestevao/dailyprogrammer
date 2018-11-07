e9d087955d9e11cb385dfcc9b963c453eb080454
Nelson Estevão <nelsonmestevao@gmail.com>
2018-11-07 21:43:07 +0000

    [Dice Roller]: Update variables name

--- dices.jl
+++ dices.jl
@@ -1,17 +1,17 @@
 #!/usr/bin/env julia
 
-function coiso(x::Int32,y::Int32)
+function calc(rolls::Int32,faces::Int32)
     sum = 0
-    for i = 1:x
-        sum += rand(1:y)
+    for i = 1:rolls
+        sum += rand(1:faces)
     end
     return sum
 end
 
 open("input.txt") do file
     for ln in eachline(file)
-        times, faces = split(ln, "d")
-        println("$(coiso(parse(Int32,times),parse(Int32,faces)))")
+        rolls, faces = split(ln, "d")
+        println("$(calc(parse(Int32,rolls),parse(Int32,faces)))")
     end
 end
 
