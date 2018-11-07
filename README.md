df4890a91713ac90ea8b973a6f72460a42a67724
Nelson Estevão <nelsonmestevao@gmail.com>
2018-11-07 21:28:57 +0000

    :zap:

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dice Roller
+
+[Problem](https://www.reddit.com/r/dailyprogrammer/comments/8s0cy1/20180618_challenge_364_easy_create_a_dice_roller/)
+
+This input resolution does not follow some of the rules in the problem
+description like the mandatory interval.
+
---
+++ dices.jl
@@ -0,0 +1,18 @@
+#!/usr/bin/env julia
+
+function coiso(x::Int,y::Int)
+    sum = 0
+    for i = 1:y
+        sum += rand(1:y)
+    end
+    return sum
+end
+
+open("input.txt") do file
+    for ln in eachline(file)
+        times, faces = split(ln, "d")
+        println("$(string(coiso(parse(Int,times),parse(Int,faces))))")
+    end
+end
+
+
