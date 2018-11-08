a0976416370acb5988b3453299a1b63f68bb4d4a
Nelson Estevão <nelsonmestevao@gmail.com>
2018-11-08 13:51:06 +0000

    [Dice Roller]: Add C version
    Co-authored-by: Hugo Carvalho <hugofilipeduartecarvalho@gmail.com>

---
+++ dices.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int dados, faces, sum, i;
+    FILE *fp = fopen("input.txt", "r+");
+
+    while(!feof(fp)) {
+        fscanf(fp, "%dd%d\n", &dados, &faces);
+
+        sum = 0;
+
+        for(i = 0; i < dados; i++)
+            sum += rand() % faces + 1;
+
+        printf("%d\n", sum);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
