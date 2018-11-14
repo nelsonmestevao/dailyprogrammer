03b5a21a5be8c82d33645d81f69ee8b215b96b14
Nelson Estevão <nelsonmestevao@gmail.com>
2018-11-14 17:56:06 +0000

    Add resolution for challenge of large input
    This change removes the print of every line and just gets a total sum
    printed out.

--- dices.jl
+++ dices.jl
@@ -1,17 +1,15 @@
 #!/usr/bin/env julia
 
-function alea_iacta_est(rolls::Int32,faces::Int32)
-    sum = 0
-    for i = 1:rolls
-        sum += rand(1:faces)
-    end
-    return sum
-end
-
 open("input.txt") do file
+    sum = 0
     for ln in eachline(file)
         rolls, faces = split(ln, "d")
-        println("$(alea_iacta_est(parse(Int32,rolls),parse(Int32,faces)))")
+        rolls = parse(Int32, rolls)
+        faces = parse(Int32, faces)
+        for i = 1:rolls
+            sum += rand(1:faces)
+        end
     end
+    println("$(sum)")
 end
 
