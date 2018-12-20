5fc0eb9045b5784e2765faf52fb7d92ea9ac9bfe
Nelson Estevão <nelsonmestevao@gmail.com>
2018-12-20 00:47:44 +0000

    [Subfactorals]: Add input file

--- subfactorials/subfactorials.jl
+++ subfactorials/subfactorials.jl
@@ -1,12 +1,20 @@
 #!/usr/bin/env julia
 
-function derangement(n::Int)
+function derangement(n::Int64)
     if n == 0
         1
     elseif n == 1
         0
     else
         (n-1) * (derangement(n-1) + derangement(n-2))
+    end
+end
+
+open("input.txt") do file
+    for ln in eachline(file)
+        #= n = parse(Int32,ln) =#
+        #= println("$(derangement(n))") =#
+        println("$(derangement((parse(Int64,ln))))")
     end
 end
 
