5c3add5baa2c0d107dd770b643f04bb23a2b7ad0
Nelson Estevão <nelsonmestevao@gmail.com>
2021-06-29 15:47:20 +0100

    Run formatter and add alternative solution

--- stair-case/staircase.hs
+++ stair-case/staircase.hs
@@ -1,13 +1,17 @@
 module Main where
 
-import System.Environment(getArgs)
+import System.Environment (getArgs)
 
 staircase :: Int -> String
-staircase n = stair n (n-1) ++ ['\n']
+staircase n = stair n (n - 1) ++ ['\n']
 
 stair :: Int -> Int -> String
 stair n 0 = replicate n '#'
-stair n a = replicate a ' ' ++ replicate (n-a) '#' ++ ['\n'] ++ stair n (a-1)
+stair n a =
+  replicate a ' ' ++ replicate (n - a) '#' ++ ['\n'] ++ stair n (a - 1)
 
 main = getArgs >>= putStr . staircase . read . head
 
+-- main = do
+--   args <- getArgs
+--   return args >>= putStr . staircase . read . head
